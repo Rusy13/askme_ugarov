fb251b074387d21b1df424efd81241d159f89595
Rusy13 <rusugarov@gmail.com>
2024-04-07 01:55:15 +0300

    [TPHW-1]: fixed bugs

--- technop/main/urls.py
+++ technop/main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup', views.signup, name='register'),
     path('question/<int:question_id>', views.question, name='question'),
     path('index', views.index, name='index'),
-    path('setting', views.setting, name='setting'),
+    path('settings', views.settings, name='settings'),
+    path('tag/<int:tag_id>', views.tag_questions, name='tag_questions'),
 ]
--- technop/main/views.py
+++ technop/main/views.py
@@ -5,6 +5,8 @@ from .models import Answer
 from .models import Tag
 from .models import Profile
 from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
 
     
 def paginate(objects, page, per_page=10):
@@ -37,9 +39,12 @@ def signup(request):
 def question(request,question_id):
     popular_tags = Tag.objects.get_popular_tags(count=5)
     popular_members = Profile.objects.get_popular_profiles(count=5)
+
     try:
+        
         quest = Question.objects.get(pk=question_id)
         ans = Answer.objects.get(pk=question_id)
+        tags = quest.tags.all()
         # ans = Answer.objects.filter(question=quest)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
@@ -48,17 +53,26 @@ def question(request,question_id):
     # item = quest[question_id-1]
     # ans = Answer.objects.all()
     # itemans = ans[question_id-1]
-    return render(request, 'question.html', {'question': quest, 'answer': ans, 'popular_tags': popular_tags, 'popular_members': popular_members})
+    return render(request, 'question.html', {'question': quest, 'answer': ans,'popular_tags': popular_tags,'tags': tags, 'popular_members': popular_members})
 
 def index(request):
     popular_tags = Tag.objects.get_popular_tags(count=5)
     popular_members = Profile.objects.get_popular_profiles(count=5)
     # popular_members = Profile.objects.get_popular_members(count=5)
     quest = Question.objects.all()
+    all_tags = [question.tags.all() for question in quest]
     page = int(request.GET.get('page', 1))
-    return render(request, 'index.html', {'questions': paginate(quest,page), 'popular_tags': popular_tags, 'popular_members': popular_members})
+    return render(request, 'index.html', {'questions': paginate(quest,page), 'popular_tags': popular_tags,'all_tags': all_tags, 'popular_members': popular_members})
 
-def setting(request):
+def settings(request):
     popular_tags = Tag.objects.get_popular_tags(count=5)
     popular_members = Profile.objects.get_popular_profiles(count=5)
-    return render(request, 'setting.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+    return render(request, 'settings.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+
+
+def tag_questions(request, tag_id):
+    # Получаем объект тега по его ID
+    tag = get_object_or_404(Tag, pk=tag_id)
+    # Получаем все вопросы, связанные с этим тегом
+    questions = tag.question_set.all()
+    return render(request, 'tag_questions.html', {'tag': tag, 'questions': questions})
