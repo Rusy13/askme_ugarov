2683ea24125bb09fe76822a20740f2e08ece6b12
Rusy13 <rusugarov@gmail.com>
2024-05-01 18:41:02 +0300

    [TPHW-1]: the necessary authorization forms have been added

--- technop/main/urls.py
+++ technop/main/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 urlpatterns = [
     path('base', views.base),
     path('ask', views.ask),
-    path('login', views.login, name='login'),
+    path('login', views.login_user, name='login'),
     path('signup', views.signup, name='register'),
     path('question/<int:question_id>', views.question, name='question'),
     path('index', views.index, name='index'),
-    path('settings', views.settings, name='settings'),
+    path('profile/edit', views.settings, name='settings'),
     path('tag/<int:tag_id>', views.tag_questions, name='tag_questions'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
--- technop/main/views.py
+++ technop/main/views.py
@@ -8,6 +8,11 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.shortcuts import redirect
     
 def paginate(objects, page, per_page=10):
     paginator = Paginator(objects, per_page)
@@ -26,15 +31,87 @@ def ask(request):
     popular_members = Profile.objects.get_popular_profiles(count=5)
     return render(request, 'ask.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
 
-def login(request):
-    popular_tags = Tag.objects.get_popular_tags(count=5)
-    popular_members = Profile.objects.get_popular_profiles(count=5)
-    return render(request, 'login.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+
+
+
+
+
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = "Invalid username or password."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        popular_tags = Tag.objects.get_popular_tags(count=5)
+        popular_members = Profile.objects.get_popular_profiles(count=5)
+        return render(request, 'login.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+
+
+
+
+
+
+
 
 def signup(request):
-    popular_tags = Tag.objects.get_popular_tags(count=5)
-    popular_members = Profile.objects.get_popular_profiles(count=5)
-    return render(request, 'signup.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+    if request.method == 'POST':
+        # Получение данных из POST-запроса
+        username = request.POST.get('login')
+        email = request.POST.get('email')
+        nickname = request.POST.get('nickname')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        avatar = request.FILES.get('avatar')
+        
+        # Проверка на совпадение паролей
+        if password != password2:
+            error_message = "Passwords do not match"
+            return render(request, 'signup.html', {'error_message': error_message})
+        
+        # Проверка на существование пользователя с таким именем
+        if User.objects.filter(username=username).exists():
+            error_message = "This username is already taken. Please choose another one."
+            return render(request, 'signup.html', {'error_message': error_message})
+        
+        # Создание нового пользователя
+        user = User.objects.create_user(username=username, email=email, password=password)
+        
+        # Другие действия с данными, например, сохранение аватара
+        
+        # Вход пользователя
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        
+        # Перенаправление на главную страницу
+        return redirect('index')
+    else:
+        return render(request, 'signup.html')
+
+
+
+
+
+
+
+
+def logout_view(request):
+    logout(request)
+    # Получаем следующий URL из параметра запроса, если он есть, или перенаправляем на главную страницу
+    next_url = request.GET.get('next', 'index')
+    return redirect(next_url)
+
+
+
 
 def question(request,question_id):
     popular_tags = Tag.objects.get_popular_tags(count=5)
@@ -70,10 +147,41 @@ def index(request):
     return render(request, 'index.html', {'questions': paginate(quest, page), 'popular_tags': popular_tags, 'all_tags': all_tags, 'popular_members': popular_members})
 
 
+
+
+
+
+
+
+
+
+
+from django.core.exceptions import ObjectDoesNotExist
+
 def settings(request):
-    popular_tags = Tag.objects.get_popular_tags(count=5)
-    popular_members = Profile.objects.get_popular_profiles(count=5)
-    return render(request, 'settings.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        login = request.POST.get('login')
+        nickname = request.POST.get('nickname')
+
+        user = request.user
+
+        user.email = email
+        user.username = login
+        user.save()
+
+        return redirect('settings')
+
+    else:
+        popular_tags = Tag.objects.get_popular_tags(count=5)
+        popular_members = Profile.objects.get_popular_profiles(count=5)
+        return render(request, 'settings.html', {'popular_tags': popular_tags, 'popular_members': popular_members})
+
+
+
+
+
+
 
 
 def tag_questions(request, tag_id):
